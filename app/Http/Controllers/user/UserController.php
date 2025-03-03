76ba6b75cf0c5d625ed6a0948bd6d25fd1063bb7
Nguyễn Văn Chỉnh <chinhbanh24@gmail.com>
2025-03-03 23:50:10 +0700

    add ctsp

--- app/Http/Controllers/user/UserController.php
+++ app/Http/Controllers/user/UserController.php
@@ -8,41 +8,57 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $products = Product::all();
         return view('user.home', compact('products'));
     }
 
-    public function pageCategory(){
+    public function pageCategory()
+    {
         $products = Product::all();
         return view('user.pageCategory', compact('products'));
     }
 
-    public function login(){
+    public function login()
+    {
         return view('user.login');
     }
 
-    public function cart(){
+    public function cart()
+    {
         return view('user.cart');
     }
 
-    public function about(){
+    public function about()
+    {
         return view('user.about');
     }
 
-    public function contact(){
+    public function contact()
+    {
         return view('user.contact');
     }
 
-    public function myAccount(){
+    public function myAccount()
+    {
         return view('user.myAccount');
     }
 
-    public function shopLeftSidebar(){
+    public function shopLeftSidebar()
+    {
         return view('user.shopLeftSidebar');
     }
 
-    public function singleProduct(){
-        return view('user.singleProduct');
+    public function singleProduct($id)
+    {
+        $product = Product::with(['variants.color', 'variants.capacity'])->findOrFail($id);
+        
+        // Lấy danh sách màu sắc và dung lượng 
+        $colors = $product->variants->pluck('color')->unique('id');
+        $capacities = $product->variants->pluck('capacity')->unique('id');
+    
+        return view('user.singleProduct', compact('product', 'colors', 'capacities'));
     }
+    
 }
