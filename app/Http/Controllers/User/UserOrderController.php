4fe44050c992969acca64b18c839b42cf9c2f5f0
nguyen nam quy <quyn34053@gmail.com>
2025-05-25 02:39:40 +0700

    ok

--- app/Http/Controllers/User/UserOrderController.php
+++ app/Http/Controllers/User/UserOrderController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductVariant;
 use Illuminate\Http\Request;
 use App\Models\Order;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+
 
 class UserOrderController extends Controller
 {
@@ -26,6 +30,24 @@ class UserOrderController extends Controller
         if (!in_array($order->status, ['pending', 'processing'])) {
             return back()->with('error', 'Bạn không thể hủy đơn hàng này.');
         }
+        // Cộng lại số lượng vào kho
+        foreach ($order->orderDetails as $detail) {
+            if ($detail->product_variant_id) {
+                // Nếu đơn hàng là sản phẩm có biến thể
+                $variant = ProductVariant::find($detail->product_variant_id);
+                if ($variant) {
+                    $variant->stock += $detail->quantity;
+                    $variant->save();
+                }
+            } else {
+                // Nếu đơn hàng là sản phẩm đơn
+                $product = Product::find($detail->product_id);
+                if ($product) {
+                    $product->quantity += $detail->quantity;
+                    $product->save();
+                }
+            }
+        }
 
         $order->status = 'cancelled';
         $order->save();
@@ -33,20 +55,19 @@ class UserOrderController extends Controller
         return redirect()->route('myAccount')->with('success', 'Đơn hàng đã được hủy.');
     }
     public function confirmReceived($id)
-{
-    $order = Order::where('id', $id)
-        ->where('user_id', auth()->id())
-        ->where('status', 'shipping')
-        ->first();
+    {
+        $order = Order::where('id', $id)
+            ->where('user_id', auth()->id())
+            ->where('status', 'shipping')
+            ->first();
 
-    if (!$order) {
-        return redirect()->back()->with('error', 'Không thể xác nhận đơn hàng.');
+        if (!$order) {
+            return redirect()->back()->with('error', 'Không thể xác nhận đơn hàng.');
+        }
+
+        $order->status = 'completed';
+        $order->save();
+
+        return redirect()->back()->with('success', 'Bạn đã xác nhận đã nhận hàng. Cảm ơn bạn!');
     }
-
-    $order->status = 'completed';
-    $order->save();
-
-    return redirect()->back()->with('success', 'Bạn đã xác nhận đã nhận hàng. Cảm ơn bạn!');
-}
-
 }
